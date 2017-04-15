34d189cc0ba6c930f4609db368c35c90e7b98c9e
chatton <cianhatton@gmail.com>
2017-04-15 15:37:02 +0100

    Added Graph.java

--- src/graph/Graph.java
+++ src/graph/Graph.java
@@ -1,4 +1,88 @@
 package graph;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 public class Graph {
-}
+
+    private final Map<Point, Location> graph;
+    private Point start;
+    private Point dest;
+
+    public List<Point> allPoints() {
+        return new ArrayList<>(graph.keySet());
+    }
+
+    public Graph(int size) { // graph will always be a square
+        this.graph = new HashMap<>();
+        for (int i = 0; i < size; i++) {
+            for (int j = 0; j < size; j++) {
+                // fill up whole graph with open spaces
+                this.graph.put(new Point(i, j), new Location(true));
+            }
+        }
+        this.start = new Point(0, 0); // default to top left
+        this.dest = new Point(size - 1, size - 1); // and bottom right
+    }
+
+    private void addLocation(Point point, Location location) {
+        this.graph.put(point, location);
+    }
+
+    public boolean isPassable(Point point) {
+        return graph.get(point).isPassable();
+    }
+
+    public void open(Point point) {
+        graph.get(point).open();
+    }
+
+    public void close(Point point) {
+        graph.get(point).close();
+    }
+
+    public void assignStart(Point startingPoint) {
+        this.start = startingPoint;
+        addLocation(startingPoint, new Location(true));
+    }
+
+    public void assignDest(Point destination) {
+        this.dest = destination;
+        this.graph.put(destination, new Location(true));
+    }
+
+    public Point start() {
+        return start;
+    }
+
+    public Point dest() {
+        return dest;
+    }
+
+    private class Location {
+        /*
+        keep track of locations in the graph, provides functionality to mark locations
+        as open/closed.
+         */
+        private boolean passable;
+
+        Location(boolean passable) {
+            this.passable = passable;
+        }
+
+        private boolean isPassable() {
+            return passable;
+        }
+
+        private void open() {
+            this.passable = true;
+        }
+
+        private void close() {
+            this.passable = false;
+        }
+    } // Location
+
+} // Graph

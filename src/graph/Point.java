6880876e0c5055b66c2e3b81a229b305dc827eed
chatton <cianhatton@gmail.com>
2017-04-15 15:04:01 +0100

    created Point class

--- src/graph/Point.java
+++ src/graph/Point.java
@@ -1,4 +1,42 @@
 package graph;
 
 public class Point {
+    private final int x;
+    private final int y;
+
+    public Point(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public int x() {
+        return x;
+    }
+
+    public int y() {
+        return y;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("{ x: %s, y: %s}", x, y);
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == this) {
+            return true;
+        }
+        if (o == null || !(o instanceof Point)) {
+            return false;
+        }
+
+        Point other = (Point) o;
+        return this.x == other.x() && this.y == other.y();
+    }
+
+    @Override
+    public int hashCode() {
+        return x * 91 ^ y / 17;
+    }
 }
